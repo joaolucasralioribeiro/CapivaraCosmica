0bad5fd5ab9dbad996e7b1e94f113cc621b7623e
Ricardo-Facio <ricardofscrignoli@gmail.com>
2025-05-27 14:58:55 -0300

    alterações
    alterações nos arquivos css, criação da página de produtos

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function mudouTamanho() {
+            if (window.innerWidth >= 750) {
+                itens.style.display = 'block'
+            } else {
+                itens.style.display = 'none'
+            }
+        }
+        function clickMenu() {
+            if (itens.style.display == 'block') {
+                itens.style.display = 'none'
+            } else {
+                itens.style.display = 'block'
+            }
+        }
+  
+        const menu = document.querySelector(".menu");
+        const header = document.querySelector(".nav");
+        menu.addEventListener("click", () => header.classList.toggle("active"));
+
+       
